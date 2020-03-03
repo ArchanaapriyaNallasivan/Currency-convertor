92fd044b135b1c10ebcc1d4b1cf95b9ecba05f1e
ArchanaapriyaNallasivan <37690300+ArchanaapriyaNallasivan@users.noreply.github.com>
2020-03-02 20:21:32 -0500

    Add Currency Convertor - Lab 6

---
+++ app/src/main/java/com/mycompany/lab6_currencyapp/CurrencyItem.java
@@ -0,0 +1,23 @@
+package com.mycompany.lab6_currencyapp;
+
+public class CurrencyItem {
+    private String base;
+    private double value;
+
+    public CurrencyItem(String base, double value)
+    {
+        this.base=base;
+        this.value=value;
+
+    }
+public String getBase()
+{
+    return this.base;
+
+}
+public double getValue()
+{
+    return this.value;
+}
+
+}
